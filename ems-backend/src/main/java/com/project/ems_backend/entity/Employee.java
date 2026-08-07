a25e5d8ad795c1cbf5977bd0d497facedb57ad0c
purusothaman v <vpurusothaman45@gmail.com>
2026-08-07 13:14:02 +0530

    Add files via upload

---
+++ ems-backend/src/main/java/com/project/ems_backend/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.project.ems_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name="employees")
+
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @Column(name="first_name")
+    private  String firstName;
+    @Column(name="last_name")
+    private  String lasttName;
+    @Column(name="email",nullable= false, unique=true)
+    private  String email;
+
+}
