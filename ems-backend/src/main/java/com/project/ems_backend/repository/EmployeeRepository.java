8db1a6e332855641f2bace37042ea6f07e67e7d2
purusoth45 <vpurusothaman45@gamil.com>
2026-08-10 14:13:12 +0530

    Add Spring Boot backend source

--- ems-backend/src/main/java/com/project/ems_backend/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.ems_backend.repository;
-
-import com.project.ems_backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
