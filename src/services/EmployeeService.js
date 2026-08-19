a59ce6b2766ebbaa2f45d670fc8a0df0b034c36c
purusoth45 <vpurusothaman45@gamil.com>
2026-08-19 11:11:05 +0530

    Update project files

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const REST_API_BASE_URL = "https://emservice.onrender.com/api/employees";
+const REST_API_BASE_URL =  "https://ems-07p3.onrender.com/api/employees";
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 export const createEmployee = (employee)=> axios.post(REST_API_BASE_URL,employee);
 export const getEmployee = (employeeId)=> axios.get(REST_API_BASE_URL + '/' +employeeId);
@@ -8,3 +8,4 @@ export const updateEmployee = (employeeId, employee) =>
   axios.put(`${REST_API_BASE_URL}/${employeeId}`, employee);
 
 export const deleteEmployee = (employeeId)=> axios.delete(REST_API_BASE_URL + '/' +employeeId);
+  
